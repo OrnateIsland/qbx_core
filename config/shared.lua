6802911f710f2dd9eae95ea53c4a144a7e0efc43
JustShoveIt <himadri.dares@gmail.com>
2024-11-28 09:14:28 +0000

    remove started item

--- config/shared.lua
+++ config/shared.lua
@@ -3,7 +3,7 @@ return {
 	defaultSpawn = vec4(-540.58, -212.02, 37.65, 208.88),
 	notifyPosition = 'bottom', -- 'top' | 'top-right' | 'top-left' | 'bottom' | 'bottom-right' | 'bottom-left'
 	---@type { name: string, amount: integer, metadata: fun(source: number): table }[]
-	starterItems = {           -- Character starting items
-		{ name = 'phone', amount = 1 },
+	starterItems = {        -- Character starting items
+		-- { name = 'phone', amount = 1 },
 	}
 }
